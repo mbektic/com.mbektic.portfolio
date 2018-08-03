2a903d736305af6123dc2561d9e0a5e5082406a9
Muamer Bektic <Bektic.Muamer@gmail.com>
2018-08-03 16:43:39 -0400

    Update 3 - The Cards: Beginning of JavaScript
    - Started JavaScript file for card generation
    - Finalized html structure
    - Cleaned up CSS

---
+++ javascript/cardGenerator.js
@@ -0,0 +1,36 @@
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// Card Structure
+
+// <div class="element-item">
+// 	<img src="images/cardBackgrounds/arrayValue1.png" alt="arrayValue2">
+//
+// 	<a href="/pages/arrayValue1.html">
+// 		<div class="titles animation-randomNumber1" style="background: transparent url("/images/cardShapes/shapeRandomNumeber2.svg") no-repeat 50% 50%/190px auto;"><span>
+// 			<h2>arrayValue3</h2>
+// 			<h3>arrayValue4</h3>
+// 		</span></div>
+// 	</a>
+// </div>
+
+
+var randomNumber1; // The number for deciding the bacground shape
+var randomNumber2;
+
+// [imageName/pageURL, imgAltTag, cardTitle, cardSubTitle]
+var contentArray = [
+	["PrintAndMail", "uofl print and main homepage", "Print and Mail", "Canon Partnership Homepage"],
+	["PrintAndMail", "uofl print and main homepage", "Print and Mail", "Canon Partnership Homepage"]
+];
+
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// Generation Function
+function generateCards(){
+	html = "";
+
+	for(i = 0; i < contentArray.length; i++){
+
+	}
+}
